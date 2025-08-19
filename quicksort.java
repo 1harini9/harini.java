9e9d20d936b20bad8e731b734ed2f9106de8efcc
1harini9 <vontimittaharini45@gmail.com>
2025-08-19 14:54:58 +0530

    Add files via upload

---
+++ quicksort.java
@@ -0,0 +1,27 @@
+public class quicksort {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quickSort(arr, low, p - 1);
+            quickSort(arr, p + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high], i = low - 1, temp;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+            }
+        }
+        temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort(arr, 0, arr.length - 1);
+        for (int n : arr) System.out.print(n + " ");
+    }
+}
